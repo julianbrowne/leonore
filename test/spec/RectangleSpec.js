d4dd5c53ab6c604da7e4fc645261a6e0ee3be7ff
Julian Browne <julianbrowne@gmail.com>
2014-08-16 11:31:33 +0100

    rectangle dimensions

--- test/spec/RectangleSpec.js
+++ test/spec/RectangleSpec.js
@@ -23,4 +23,28 @@ describe("Rectangle", function() {
     expect(shape.type).toEqual('rect');
   });
 
+  it("should set correct height", function() { 
+    shape.height(50);
+    expect(shape.height()).toEqual(50);
+  });
+
+  it("should set correct width", function() { 
+    shape.width(50);
+    expect(shape.width()).toEqual(50);
+  });
+
+  it("should set correct dimensions", function() { 
+    shape.dimensions({height: 12, width: 34});
+    expect(shape.width()).toEqual(34);
+    expect(shape.height()).toEqual(12);
+  });
+
+  it("should fetch correct dimensions", function() { 
+    shape.dimensions({height: 55, width: 99});
+    var dim = shape.dimensions();
+    expect(Object.keys(dim).length).toEqual(2);
+    expect(dim.height).toEqual(55);
+    expect(dim.width).toEqual(99);
+  });
+
 });
